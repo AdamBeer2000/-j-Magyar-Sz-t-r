6fb5882413ec657f0440c594c7d0f982016a1f24
AdamBeer2000 <57035872+AdamBeer2000@users.noreply.github.com>
2021-02-22 21:27:02 +0100

    TestProject (ASP.NET )

---
+++ WebNewmagyarszotar/index.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebNewmagyarszotar.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="background-image: url('https://i.imgur.com/xhfoKn4.png')">
+    <img src="https://i.imgur.com/qwR2B46.png" alt="itt kéne lennie">
+    <form id="form1" runat="server">
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        <asp:Label ID="Label1" runat="server" BackColor="White" Text="Még semmi"></asp:Label>
+    </form>
+ </body>
+</html>
---
+++ WebNewmagyarszotar/index.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebNewmagyarszotar
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=tcp:the-first-git-emire.database.windows.net,1433;Initial Catalog=NewMagyarSzotar;Persist Security Info=False;User ID=pistabacsi;Password=Nemezajelszo1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                con.Open();
+                Label1.Text = "Fasza :)";
+                con.Close();
+            }
+            catch
+            {
+                Label1.Text = "Nem Fasza :(";
+            }
+            
+        }
+    }
+}
