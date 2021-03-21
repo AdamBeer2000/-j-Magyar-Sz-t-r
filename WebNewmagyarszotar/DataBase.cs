c6cd43e2f355d0cd7b140fb03ec8dca0d4d447be
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-03-21 13:54:14 +0100

    szoosztalyok
    DataBase.cs lehet oof lesz

--- WebNewmagyarszotar/DataBase.cs
+++ WebNewmagyarszotar/DataBase.cs
@@ -32,5 +32,7 @@ namespace WebNewmagyarszotar
 
             return result;
         }
+
+        
     }
 }
--- WebNewmagyarszotar/index.aspx.cs
+++ WebNewmagyarszotar/index.aspx.cs
@@ -20,6 +20,7 @@ namespace WebNewmagyarszotar
             {
                 //YEY ->Connection check
             }
+            //connection_info = db.getAllDataDEBUG();
         }
 
         protected void Button1_Click(object sender, EventArgs e)
