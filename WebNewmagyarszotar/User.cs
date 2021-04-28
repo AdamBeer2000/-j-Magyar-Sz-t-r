5f21212b03c74680c6cff04612f0cbb651d27cf2
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-04-28 10:17:37 +0200

    Users.cs hozzáadva a felhasználó pillanatnyi tárolásához
    A User.cs osztály csak adatot tárol, illetve majd a jövőben hozzáadhatunk egyéb funkciókat is, de csak egy egységes tárolási opciónak készült el!

---
+++ WebNewmagyarszotar/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebNewmagyarszotar
+{
+    public class User
+    {
+        //csak adatot tárol!
+        //username
+        //permission
+        private string username = "";
+        //private int/enum permission = ?
+
+        public string Username
+        {
+            get { return this.username; }
+            set { this.username = value; }
+        }
+
+        public User() { }
+
+    }
+}
