a7f461cb021ddd7b4847cc67ee6e24e9a073305e
Macska kapitany <42695936+mcmacska@users.noreply.github.com>
2021-05-18 15:07:43 +0200

    delete gomb

--- WebNewmagyarszotar/index.aspx.designer.cs
+++ WebNewmagyarszotar/index.aspx.designer.cs
@@ -76,5 +76,14 @@ namespace WebNewmagyarszotar
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlImage engword;
+
+        /// <summary>
+        /// deletebutton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton deletebutton1;
     }
 }
